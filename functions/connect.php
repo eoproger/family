23503a699111486ddb07436e476de28d83424234
EO_proger <eostudio@yandex.ru>
2022-08-05 19:28:37 +0400

    tree v1

--- functions/connect.php
+++ functions/connect.php
@@ -4,7 +4,7 @@ $user = "u1281236_eoprog";
 $password = "MVBsFv9Q4bGkhMK";
 $host = "localhost";
 $db = "u1281236_family_base";
-$dbh = 'mysql:host='.$host.';dbname='.$db.'charset=utf8';
+$dbh = 'mysql:host='.$host.';dbname='.$db.';charset=utf8';
 $pdo = new PDO($dbh,$user,$password);
 
 ?>
