bd208f0542b8c031c367c6696a5e331e8784450f
EO_proger <eostudio@yandex.ru>
2022-08-05 19:17:26 +0400

    tree v1

--- admin/admin.php
+++ admin/admin.php
@@ -1,5 +1,18 @@
 <?
 require_once "../functions/connect.php";
+session_start();
 
 $login = $_POST["login"];
 $password = $_POST["password"];
+
+$sql = $pdo->prepare("SELECT id, login FROM user WHERE login=:login AND password=:password");
+$sql->execute(array('login' => $login, 'password' => $password));
+$array=$sql->fetch(PDO::FETCH_ASSOC);
+
+if($array["id"] > 0){
+    $_SESSION['login'] = $array["login"];
+    header('locarion: /admin.php');
+}else{
+    header('locarion: /login.php');
+}
+?>
