76a3f0dddd3a0473452a0275d9d63be66ed1e1ff
EO_proger <eostudio@yandex.ru>
2022-08-05 22:10:44 +0400

    tree v1

--- admin.php
+++ admin.php
@@ -1,6 +1,27 @@
-<?
-session_start();
+<?session_start();?>
 
-echo "Вы в админке";
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Вход в админку</title>
+</head>
+<body>
+    <?
+    if(!empty($_SESSION['login'])){?>
+       <a href="/logout.php">Выйти из админки</a>
+    <?
+    }else{?>
+        <h3>Увас нет доступа. Нужно авторизоваться как администратор</h3>
+        <a href="/login.php">На страницу авторизации</a>
+        <a href="/index.php">На главную</a>
+    <?
+    }
+    ?>
 
-?>
+</body>
+</html>
+
