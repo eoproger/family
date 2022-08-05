900aaf4827c75e8c2d3029e4d51a8f42fc40cc00
EO_proger <eostudio@yandex.ru>
2022-08-05 18:48:29 +0400

    tree v1

---
+++ login.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Вход в админку</title>
+</head>
+<body>
+    <h1>Вход в админку</h1>
+    <form action="admin/admin.php" method="post">
+        <div class="form-group">
+            <input type="text" placeholder="Введите логин">
+            <input type="text" placeholder="Введите пароль">
+            <button type="submit">Вход</button>
+        </div>
+    </form>
+</body>
+</html>
