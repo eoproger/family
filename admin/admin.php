8fca79e6deab730be5c7533d4fe7cb5fa8383a4a
EO_proger <eostudio@yandex.ru>
2022-08-05 19:32:31 +0400

    tree v1

--- admin/admin.php
+++ admin/admin.php
@@ -1,11 +1,11 @@
 <?
-require_once "../functions/connect.php";
+require_once '../functions/connect.php';
 session_start();
 
 $login = $_POST["login"];
 $password = $_POST["password"];
 
-$sql = $pdo->prepare("SELECT id, login FROM user WHERE login=:login AND password=:password");
+$sql = $pdo->prepare("SELECT id,login FROM user WHERE login=:login AND password=:password");
 $sql->execute(array('login' => $login, 'password' => $password));
 $array=$sql->fetch(PDO::FETCH_ASSOC);
 
